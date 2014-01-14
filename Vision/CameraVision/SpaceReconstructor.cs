eab15753fb7874a9ef8ff6481838531922c24bbf
Vitalii <pomoinytskyi@hotmail.com>
2014-01-15 01:21:14 +0200

    Add CameraVision project and test project for it.

---
+++ Vision/CameraVision/SpaceReconstructor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using AForge.Math;
+
+namespace CameraVision
+{
+    public class SpaceReconstructor
+    {
+        public List<Vector3> Reconstruct3DSpace(Bitmap first, Bitmap second)
+        {
+            return null;
+        }
+    }
+}
