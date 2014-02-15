d78a8b53a253e856dab630870026e137a852b435
pomoinytskyi <pomoinytskyi@hotmail.com>
2014-02-15 04:20:08 +0200

    Add Raspberry I2C driver
    Add first test sample for servo

---
+++ Mecanics/RaspberryPi/RaspberryPi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using RPi.I2C.Net;
+
+namespace RaspberryPi
+{
+    class Program
+    {
+        private const string I2CBusPath = @"/dev/i2c-1";
+        private const int BusAdress = 40;
+
+        static void Main(string[] args)
+        {
+            using (var bus = I2CBus.Open(I2CBusPath))
+            {
+                Console.WriteLine("Will start");
+                bus.WriteByte(BusAdress, 1);
+                Console.WriteLine("Done.");
+                Console.Read();
+            }
+        }
+    }
+}
