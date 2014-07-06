b5b85a8fdc3ec362b74a420bf58a0beb6b36928b
Vitalii Pomoinytskyi <Vitalii.Pomoinytskyi@tobii.com>
2014-07-06 19:50:18 +0300

    Arduino driver with Motor configuration parser

--- ArduinoMicro/ArduinoDriver/ArduinoDriver.ino
+++ ArduinoMicro/ArduinoDriver/ArduinoDriver.ino
@@ -1,6 +1,5 @@
 #include <Wire.h>
 #include <Dagu4Motor.h>
-
 /*************************************************************************************
 i2c - http://blog.oscarliang.net/raspberry-pi-arduino-connected-i2c/
 Notes
@@ -8,6 +7,30 @@ pwmPin: Digital pin to set motor speed
 dirPin: Digital pin to set motor direction
 currPin: Analog pin to monitor current usage 
 *************************************************************************************/
+
+#define InvalidMessageType 0 
+#define PingMessageType 1 
+#define MotorsDataMessageType 2 
+#define GetStatusMessageType 3 
+#define NewConfiguration 4
+
+struct MotorData{
+	byte MotorId;
+	byte Direction;
+	byte Speed;
+};
+
+struct Message{
+public:
+  bool InProcessing;
+  byte messageType;  
+  MotorData* Motors;
+public:  
+  Message(){
+    Motors = new MotorData[4]();
+  }
+};
+
 // define slave address (0x2A = 42)
 #define SLAVE_ADDRESS 0x2A
 
@@ -36,20 +59,21 @@ Dagu4Motor* frontLeft;
 Dagu4Motor* backRight;
 Dagu4Motor* backLeft;
 
+String space = ", ";  
 bool receivingData = false;
-int* message;
+int* messageBytes;
 int messageLength = 0;
-int received = 0;
+byte received = 0;
 bool processed = true;
+Message message;
 
 //======================================= S E T U P ===============================================
 void setup(){ 
-        delay(2000);
+    delay(2000);
 	Serial.begin(9600);
 	InitializeI2C();
-	InitializeMotors();
-  
-	pinMode(13, OUTPUT); //?????????????????/  	
+	InitializeMotors();    
+	pinMode(13, OUTPUT);
 }
 
 void InitializeI2C(){
@@ -60,9 +84,8 @@ void InitializeI2C(){
 }
 
 void InitializeMotors(){	
-	Serial.println("Motors initialization");
+	Serial.println("Motors initialization");	
 	
-	String space = ", ";  
 	String vhileName = "Front-Right ";
 	frontRight =   new Dagu4Motor(FrontRightPwmPin,  FrontRightDirPin, FrontRightCurPin);
 	Serial.println(vhileName + FrontRightPwmPin + space + FrontRightDirPin + space + FrontRightCurPin);
@@ -89,56 +112,113 @@ void InitializeMotors(){
 // callback for received data
 void OnReceiveData(int byteCount) 
 { 
-  receivingData = true;    
-  free(message);
-  messageLength = 0;
+	if(byteCount == 0)
+	{
+		return;
+	}
+	receivingData = true;
+	messageLength = 0;
   
   Serial.print("Receive" );
   Serial.println(byteCount);
   
-  message = (int*)calloc(byteCount, sizeof(int));
+  messageBytes = (int*)calloc(byteCount, sizeof(int));
   messageLength = byteCount;
   int received = 0;
   while(Wire.available())
   {
-    message[received] = Wire.read();    
-    digitalWrite(13, HIGH); 
-    delay(200); 
-    digitalWrite(13, LOW);    
+    messageBytes[received] = Wire.read();
     received++;
   }    
   processed = false;
-  receivingData = false;
+  receivingData = false;  
 }
 
 void OnRequestData(){
-	 Wire.write(received - 1);
+  Wire.write(message.messageType);
 }
 
 bool direction = false;
+
+void ParseNewMotorState(){
+	Serial.println("Parsing motors state");
+	// If MessageType = MotorsDataMessageType; DataLength => 4*3 = 12. TotalLength = 3+12 = 15
+	if(messageBytes[0] != 15) {
+		message.messageType = InvalidMessageType;		
+		return;
+	}	
+	message.Motors[messageBytes[2]].MotorId = messageBytes[2];
+	message.Motors[messageBytes[2]].Direction = messageBytes[3];
+	message.Motors[messageBytes[2]].Speed = messageBytes[4];
+	
+	message.Motors[messageBytes[5]].MotorId = messageBytes[5];
+	message.Motors[messageBytes[5]].Direction = messageBytes[6];
+	message.Motors[messageBytes[5]].Speed = messageBytes[7];
+	
+	message.Motors[messageBytes[8]].MotorId = messageBytes[8];
+	message.Motors[messageBytes[8]].Direction = messageBytes[9];
+	message.Motors[messageBytes[8]].Speed = messageBytes[10];
+	
+	message.Motors[messageBytes[11]].MotorId = messageBytes[11];
+	message.Motors[messageBytes[11]].Direction = messageBytes[12];
+	message.Motors[messageBytes[11]].Speed = messageBytes[13];
+
+        PrintMotorState();
+}
+
+void PrintMotorState(){
+  	Serial.println("Motors state");
+	for(int i = 0; i < 4; i++){		
+		Serial.println(message.Motors[i].MotorId + space + message.Motors[i].Direction + space + message.Motors[i].Speed);
+	}
+}
+
+/* [0] - Length
+   [1] - MessageType
+   [2 - n-2] - message data
+   [n-1] - Check sum 
+*/
+void ProcessMessage(){
+
+	if(messageLength > 0 && !processed){
+		message.InProcessing = true;
+		
+		if (messageBytes[0] != messageLength){
+			message.messageType = InvalidMessageType;
+			message.InProcessing = false;
+			return;
+		}
+		
+		message.messageType = messageBytes[1];
+                String messageType = "Message type - ";
+                Serial.println(messageType + message.messageType);
+		
+		if(message.messageType == MotorsDataMessageType)
+                {
+	          ParseNewMotorState();
+		}
+		
+		byte checkSum = messageBytes[messageLength-1];
+		processed = true;
+		message.InProcessing = false;
+                free(messageBytes);
+                messageLength = 0;
+	  }
+}
 
 void loop()
 {  
   // Verify are all data received;
   if(receivingData)
   {
-    Serial.println("Data not received");
+    Serial.println("Wait for data");
     delay(10);
     return;
   }
   
-  if(messageLength > 0 && !processed){
-    Serial.print("Received ");
-    for(int i = 0; i< messageLength; i++)
-    {
-      Serial.print(message[i]);
-    }
-    Serial.println(".");
-    processed = true;
-  }
+  ProcessMessage();
   
-  /*direction = !direction;
-  Serial.println("New Loop");
+/*  Serial.println("New Loop");
   
   motor1->stopMotors();
   motor2->stopMotors();
@@ -160,7 +240,5 @@ void loop()
   motor1->stopMotors();
   motor2->stopMotors();
   motor3->stopMotors();
-  motor4->stopMotors();
-  */
+  motor4->stopMotors();  */
 }
-
